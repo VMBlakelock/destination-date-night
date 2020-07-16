e54e901a944dae501037c0da8d4a0c1e3519bb00
Vickie <Vickie.Blakelock@gmail.com>
2020-07-16 20:30:17 +0000

    JS - added more if statements and get elementbyID

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,9 +12,25 @@ function getDate() {
 
 // place date is going 
 
-if (act_result === "dinner" + city_result === "leeds") {
+if (act_result === "dinner" && city_result === "leeds") {
     destination = "Nando's in Leeds City Centre";
 }
+if (act_result === "dinner" + city_result === "manchester") {
+    destination = "McDonald's in Manchester City Centre";
+}
+if (act_result === "dinner" + city_result === "london") {
+    destination = "Gordon Ramsay's Bread Street Kitchen in London";
+}
+
+document.getElementById ("result").innerHTML =
+"You have chosen " + act_result + " activity " + " in the city of " + city_result;
+
+document.getElementById ("destination").innerHTML =
+"Your going to " + destination;
+
+
+
+
 
 
 
