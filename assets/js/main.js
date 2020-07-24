17bab69f7b38f1f957f10707e62fdf520495f1ae
Vickie <Vickie.Blakelock@gmail.com>
2020-07-24 21:33:23 +0000

    no changes cant get the map or button to work, keep getting null error

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,14 @@
-// assistance from mentor 
+
 let act_result ="";
 let city_result ="";
 
-function getDate() {
-    let act = document.getElementById("activities");
-    let act_result = act.options[act.selectedIndex].value;
+function getDate(){
+  let act = document.getElementById("activities");
+  let act_result = act.options[act.selectedIndex].value;
 
-    let city = document.getElementById("cities");
-    let city_result = city.options[city.selectedIndex].value;
+  let city = document.getElementById("cities");
+  let city_result = city.options[city.selectedIndex].value;
 }
-
 let destination ='';
 
 // place date is going 
@@ -17,9 +16,11 @@ let destination ='';
 if (act_result === "dinner" && city_result === "manchester") {
     destination = "McDonald's in Manchester City Centre";
 }
+
 if (act_result === "dinner" && city_result === "leeds") {
     destination = "Nando's in Leeds City Centre";
 }
+
 if (act_result === "dinner" && city_result === "london") {
     destination = "Gordon Ramsay's Bread Street Kitchen in London";
 }
@@ -28,24 +29,26 @@ if (act_result === "activity" && city_result === "leeds") {
     destination = "Mud race in Leeds City Center";
 }
 
-document.getElementById("result").innerHTML =
-"You have chosen " + act_result + " activity " + "in the city of " + city_result;
+document.getElementById("result").innerHTML = `You have chosen ${act_result}activity in the city of ${city_result}`;
 
-document.getElementById("destination").innerHTML =
-"Your going to " + destination;
-
+document.getElementById("destination").innerHTML = `Your going to ${destination}`;
 
 
 // google maps 
 
 let map;
+
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     zoom: 5,
     center: { lat: 54.230956, lng: -8.9539 }
   });
+new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Hello World!"
+  });
 }
-
 
 // code for obfuscated version of my api key - from tutor support Tim. https://github.com/Code-Institute-Org/key-helper
 
